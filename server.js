ece0ed89c107693e2f6e266235f313f31935e3ec
robin kim <robsookim@gmail.com>
2019-02-02 11:00:24 -0500

    trying to fix added files

--- server.js
+++ server.js
@@ -1,47 +1,37 @@
-require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
+//Dependencies
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 
-var db = require("./models");
-
+// Create an instance of the express app.
 var app = express();
+
+// set port to 3000 or whatever heroku (deployment site) sets it to
 var PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
+// express middleware needed for serving static files.
+app.use(express.static(__dirname + '/public'));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+/// bodyparsers 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
+
+// override with POST having ?_method=DELETE or PUT
+app.use(methodOverride('_method'));
+
+// Set Handlebars as the default templating engine.
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// now import the routes
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
 
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// Initiate the listener.
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
-
-module.exports = app;
